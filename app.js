6499e10c45225442e22ec25ca62d3cc4c3cfc051
Spades Server <spadesIT1920@gmail.com>
2019-10-02 21:31:30 +0000

    API version 1 [Server Ready] (Fixed browser API access)

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Origin, Accept');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.header('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
+
 app.use('/api', mainRoute);
 //app.use(validator.validate());
 

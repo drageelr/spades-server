ce106a0270bf80af5560f8ebe518f9d4a909dacf
Hammad Nasir <hammadn99@gmail.com>
2019-10-11 11:43:53 +0500

    API version 4 [Server Ready] (Data Scraper Fix)

--- services/datascraper.js
+++ services/datascraper.js
@@ -18,33 +18,36 @@ exports.AssembleData = async (req, res, next) =>
             {
                 resObj.teams[i][x] = teams[i][x];
             }
-            resObj.teams[i].inst = {};
-            let inst = await Inst.findOne({teamID: teams[i]._id}, '-_id -__v');
-            for(let x in inst)
+            if(teams[i].registered)
             {
-                resObj.teams[i].inst[x] = inst[x];
-            }
-            resObj.teams[i].members = [];
-            let members = await Member.find({teamID: teams[i]._id}, '-__v');
-            for(let j = 0; j < members.length; j++)
-            {
-                resObj.teams[i].members[j] = {};
-                for(let x in members[j])
-                {
-                    if(x != '_id')
+                resObj.teams[i].inst = {};
+                    let inst = await Inst.findOne({teamID: teams[i]._id}, '-_id -__v');
+                    for(let x in inst)
                     {
-                        resObj.teams[i].members[j][x] = members[j][x];
+                        resObj.teams[i].inst[x] = inst[x];
                     }
-                    resObj.teams[i].members[j][x] = members[j][x].toString();
+                    resObj.teams[i].members = [];
+                    let members = await Member.find({teamID: teams[i]._id}, '-__v');
+                    for(let j = 0; j < members.length; j++)
+                    {
+                        resObj.teams[i].members[j] = {};
+                        for(let x in members[j])
+                        {
+                            if(x != '_id')
+                            {
+                                resObj.teams[i].members[j][x] = members[j][x];
+                            }
+                            resObj.teams[i].members[j][x] = members[j][x].toString();
+                        }
+                    }
+                    resObj.teams[i].event = {};
+                    let event = await Event.findOne({teamID: teams[i]._id}, '-_id -__v');
+                    for(let x in event)
+                    {
+                        resObj.teams[i].event[x] = event[x]; 
+                    }
                 }
             }
-            resObj.teams[i].event = {};
-            let event = await Event.findOne({teamID: teams[i]._id}, '-_id -__v');
-            for(let x in event)
-            {
-                resObj.teams[i].event[x] = event[x]; 
-            }
-        }
 
         res.json(resObj);
     }

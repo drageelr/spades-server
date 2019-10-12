a6819a179bc5d2ed1166cb3b40a2fce868fa4a60
Hammad Nasir <hammadn99@gmail.com>
2019-10-12 06:00:20 +0500

    API version 5 [Server Ready] (Limit 50mb)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var datascraper = require('./services/datascraper');
 var app = express();
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 

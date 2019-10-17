8a7d873b01c18344836f0f2fafdbae0352c24ec2
Hammad Nasir <hammadn99@gmail.com>
2019-10-17 20:48:06 +0500

    API version 7 [Server Ready] (Fixes)

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -86,6 +86,12 @@ exports.search = async (req, res, next) =>
                 }
             }
 
+            // Printing Sorted Stuff:
+            for(let i = 0; i < teamIDs.length; i++)
+            {
+                console.log('idArr[' + i + ']: ' + ' teamID: ' + teamIDs[i].teamID + ' index: ' + teamIDs[i].index);
+            }
+
 
             for(let i = 0; i < teams.length; i++)
             {

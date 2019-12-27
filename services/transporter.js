240ce3a9af2f2c2a4f705a1f4f7aef2041573f54
Hammad Nasir <hammadn99@gmail.com>
2019-12-27 23:54:00 +0500

    API version 16 [Server Ready] (Event Allotment API)

--- services/transporter.js
+++ services/transporter.js
@@ -93,7 +93,7 @@ exports.sendEventAllotment = (emailTarget, teamID, eventsAllotted, eventCount) =
         from: 'noreply <noreplyspades@gmail.com>',
         to: 'spadesIT1920@gmail.com', //emailTarget,
         subject: 'Event Allotment',
-        html: `<div><h1>Spades Event Allotment</h1><b><p>Team ID: ${teamID} <br> Events Allotted: ${eventCount} <br> Logical: ${eventsAllotted.logical} <br> Mystery: ${eventsAllotted.mystery} <br> Engineering: ${eventsAllotted.engineering} <br> Drogone: ${eventsAllotted.logical} </p></b></div>`
+        html: `<div><h1>Spades PSIFI Event Allotment</h1><b><p>Team ID: ${teamID} <br> Events Allotted: ${eventCount} <br> Logical: ${eventsAllotted.logical} <br> Mystery: ${eventsAllotted.mystery} <br> Engineering: ${eventsAllotted.engineering} <br> Drogone: ${eventsAllotted.drogone} </p></b></div>`
     };
 
     transporter.sendMail(mailOptions, function(error, info){
